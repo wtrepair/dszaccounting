01cee71430f7f347a0e8c0dfc78aad53c63e995e
Reh1t <91369084+Reh1t@users.noreply.github.com>
2025-02-15 12:54:46 +0500

    update build output directory to 'dist' and add emptyOutDir option; add initial SVG assets and index.html

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: "dist", // Change "build" to "dist" to match Vercel's expected directory
+    emptyOutDir: true, // Ensures the folder is cleaned before each build
   },
   plugins: [react()],
 });
